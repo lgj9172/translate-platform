a7ec51f7ad054ed969bc7b6383da5d61f110415d
GJ <lgj9172@gmail.com>
2023-06-17 20:39:24 +0900

    add: signin page markup

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -1,29 +1,52 @@
+import GoogleLogo from "@assets/logos/signin-google.svg";
+import KakaoLogo from "@assets/logos/signin-kakao.svg";
+import NaverLogo from "@assets/logos/signin-naver.svg";
+import Typography from "@/components/Typography";
+
 export default function Signin() {
   return (
     <div className="flex-1 h-full w-full flex flex-col justify-center items-center">
       <div>
         <div className="mb-[40px] flex flex-col items-center">
-          <p>투명한 가격비교</p>
-          <p>검증된 번역사</p>
+          <Typography type="title-20" bold align="center">
+            <span className="text-[#F8730A]">투명한 가격비교</span>
+            <br />
+            검증된 번역사
+          </Typography>
         </div>
         <div className="flex flex-col justify-center items-stretch gap-3">
           <button
             type="button"
-            className="w-[327px] py-[16px] bg-[#03C75A] rounded-[8px] font-[700] text-[16px] leading-[24px] tracking-[-0.004em] text-[#FFFFFF]"
+            className="relative w-[320px] py-[16px] bg-[#03C75A] rounded-[8px] flex justify-center items-center"
           >
-            네이버로 시작하기
+            <span className="absolute left-[20px]">
+              <NaverLogo />
+            </span>
+            <span className="font-[700] text-[16px] leading-[24px] tracking-[-0.004em] text-[#FFFFFF]">
+              네이버로 시작하기
+            </span>
           </button>
           <button
             type="button"
-            className="w-[327px] py-[16px] bg-[#F9E000] rounded-[8px] font-[700] text-[16px] leading-[24px] tracking-[-0.004em] text-[#000000]"
+            className="relative w-[320px] py-[16px] bg-[#F9E000] rounded-[8px] flex justify-center items-center"
           >
-            카카오톡으로 시작하기
+            <span className="absolute left-[20px]">
+              <KakaoLogo />
+            </span>
+            <span className="font-[700] text-[16px] leading-[24px] tracking-[-0.004em] text-[#000000]">
+              카카오톡으로 시작하기
+            </span>
           </button>
           <button
             type="button"
-            className="w-[327px] py-[16px] bg-[#000000] rounded-[8px] font-[700] text-[16px] leading-[24px] tracking-[-0.004em] text-[#FFFFFF]"
+            className="relative w-[320px] py-[16px] bg-[#FFFFFF] rounded-[8px] flex justify-center items-center border border-[#D7D8D9]              "
           >
-            애플로 시작하기
+            <span className="absolute left-[20px]">
+              <GoogleLogo />
+            </span>
+            <span className="font-[700] text-[16px] leading-[24px] tracking-[-0.004em] text-[#000000]">
+              Google로 시작하기
+            </span>
           </button>
         </div>
       </div>
---
+++ components/Typography.tsx
@@ -0,0 +1,48 @@
+import classNames from "classnames";
+import { ReactNode } from "react";
+
+interface TypographyProps {
+  type:
+    | "error"
+    | "body-10"
+    | "body-12"
+    | "body-14"
+    | "body-16"
+    | "title-18"
+    | "title-20"
+    | "title-24"
+    | "title-28";
+  children: ReactNode;
+  bold?: boolean;
+  align?: "left" | "right" | "center";
+}
+
+export default function Typography({
+  type,
+  children,
+  bold = false,
+  align = "left",
+}: TypographyProps) {
+  return (
+    <p
+      className={classNames({
+        "font-[400]": !bold,
+        "font-[700]": bold,
+        "text-left": align === "left",
+        "text-center": align === "center",
+        "text-right": align === "right",
+        "text-[12px] tracking-[-0.4] leading-[24px]": type === "error",
+        "text-[10px] tracking-[-0.4] leading-[16px]": type === "body-10",
+        "text-[12px] tracking-[-0.4] leading-[18px]": type === "body-12",
+        "text-[14px] tracking-[-0.6] leading-[24px]": type === "body-14",
+        "text-[16px] tracking-[-0.4] leading-[24px]": type === "body-16",
+        "text-[18px] tracking-[-0.4] leading-[28px]": type === "title-18",
+        "text-[20px] tracking-[-0.5] leading-[30px]": type === "title-20",
+        "text-[24px] tracking-[-0.6] leading-[40px]": type === "title-24",
+        "text-[28px] tracking-[-0.8] leading-[48px]": type === "title-28",
+      })}
+    >
+      {children}
+    </p>
+  );
+}

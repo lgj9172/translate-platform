382ff1867fe7aea72c0ace56ff8c968bae8397c0
GJ <lgj9172@gmail.com>
2023-07-07 00:06:45 +0900

    add: chakra ui

--- utils/react-query/Provider.tsx
+++ utils/react-query/Provider.tsx
@@ -1,8 +1,14 @@
 "use client";
 
+import { CacheProvider } from "@chakra-ui/next-js";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import React from "react";
+
+const colors = {};
+
+export const theme = extendTheme({ colors });
 
 function Providers({ children }: React.PropsWithChildren) {
   const [client] = React.useState(
@@ -10,10 +16,14 @@ function Providers({ children }: React.PropsWithChildren) {
   );
 
   return (
-    <QueryClientProvider client={client}>
-      {children}
-      <ReactQueryDevtools initialIsOpen={false} />
-    </QueryClientProvider>
+    <CacheProvider>
+      <ChakraProvider theme={theme}>
+        <QueryClientProvider client={client}>
+          {children}
+          <ReactQueryDevtools initialIsOpen={false} />
+        </QueryClientProvider>
+      </ChakraProvider>
+    </CacheProvider>
   );
 }
 

10a32f9d98204d9073f9f93d54f19f3a2db40cc5
GJ <lgj9172@gmail.com>
2025-01-02 04:24:06 +0900

    fix: object to form data util

--- utils/converter/form.ts
+++ utils/converter/form.ts
@@ -1,8 +1,19 @@
-export const objectToFormData = (object: any) => {
+export const objectToFormData = <T extends object>(object: T) => {
   const formData = new FormData();
+
   Object.entries(object).forEach(([key, value]) => {
-    if (value === "") formData.append(key, "");
-    else formData.append(key, String(value));
+    if (value === null || value === undefined) {
+      return;
+    }
+
+    if (value instanceof File) {
+      formData.append(key, value);
+    } else if (value === "") {
+      formData.append(key, "");
+    } else {
+      formData.append(key, String(value));
+    }
   });
+
   return formData;
 };

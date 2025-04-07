919a364aff1b690987fc6f73e7feee7eb1fd9a7d
GJ <lgj9172@gmail.com>
2025-04-07 21:18:31 +0900

    [KAN-271] reset user information after signout

--- hooks/useUser.ts
+++ hooks/useUser.ts
@@ -1,9 +1,11 @@
 import { getUser, postLogout } from "@/apis/user";
-import { useMutation, useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import { useRouter } from "next/navigation";
 
 export default function useUser() {
   const router = useRouter();
+
+  const queryClient = useQueryClient();
 
   const { data: user } = useQuery({
     queryKey: ["users", "me"],
@@ -13,6 +15,9 @@ export default function useUser() {
   const { mutate: signOut } = useMutation({
     mutationFn: postLogout,
     onSuccess: () => {
+      queryClient.removeQueries({
+        queryKey: ["users", "me"],
+      });
       router.push("/");
     },
   });

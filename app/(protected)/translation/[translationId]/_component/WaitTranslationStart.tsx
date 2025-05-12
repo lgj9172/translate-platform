6f5ffc66c1a7df976b99040daeb337ce6bb87395
GJ <lgj9172@gmail.com>
2025-05-12 23:50:02 +0900

    feat: cancel translation in wait translation start

--- app/(protected)/translation/[translationId]/_component/WaitTranslationStart.tsx
+++ app/(protected)/translation/[translationId]/_component/WaitTranslationStart.tsx
@@ -1,47 +1,22 @@
 "use client";
 
-import { postTranslationCancel } from "@/apis/translations";
 import Button from "@/components/Button";
+import CancelTranslationModal from "@/modals/CancelTranslationModal";
 import { Translation } from "@/types/entities";
 import { Stack } from "@mantine/core";
-import { modals } from "@mantine/modals";
-import { useMutation } from "@tanstack/react-query";
+import { useState } from "react";
 
 interface Props {
   translation: Translation;
 }
 
 export default function WaitTranslationStart({ translation }: Props) {
-  const { mutate: mutatePostTranslationCancel } = useMutation({
-    mutationFn: postTranslationCancel,
-    onSuccess: () => {},
-  });
+  const [openCancelTranslationModal, setOpenCancelTranslationModal] =
+    useState(false);
 
-  const handleClickCancelTranslation = () =>
-    modals.open({
-      title: <div className="text-lg font-bold">번역 요청 취소</div>,
-      children: (
-        <div className="flex flex-col gap-2">
-          <div>아직 변역이 시작되지 않았다면 취소 할 수 있어요.</div>
-          <div className="flex justify-end gap-2">
-            <Button variant="secondary" onClick={() => modals.closeAll()}>
-              닫기
-            </Button>
-            <Button
-              variant="primary"
-              onClick={() => {
-                mutatePostTranslationCancel({
-                  translationId: translation.translation_id,
-                });
-                modals.closeAll();
-              }}
-            >
-              번역 요청 취소
-            </Button>
-          </div>
-        </div>
-      ),
-    });
+  const handleClickCancelTranslation = () => {
+    setOpenCancelTranslationModal(true);
+  };
 
   return (
     <Stack>
@@ -60,6 +35,11 @@ export default function WaitTranslationStart({ translation }: Props) {
           번역 요청 취소
         </Button>
       </div>
+      <CancelTranslationModal
+        open={openCancelTranslationModal}
+        onOpenChange={setOpenCancelTranslationModal}
+        translationId={translation.translation_id}
+      />
     </Stack>
   );
 }

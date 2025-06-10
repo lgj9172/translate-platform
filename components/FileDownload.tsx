8a945f5a7c73b65655e292543d6fe7edbb8cd5a6
GJ <lgj9172@gmail.com>
2025-06-11 01:09:06 +0900

    feat: file download

--- components/FileDownload.tsx
+++ components/FileDownload.tsx
@@ -1,15 +1,28 @@
 import { getFile } from "@/apis/files";
+import { useFileDownload } from "@/hooks/useFileDownload";
 import { useQuery } from "@tanstack/react-query";
 
 export default function FileDownload({ fileId }: { fileId: string }) {
+  const { downloadFile } = useFileDownload();
+
   const { data: file } = useQuery({
     queryKey: ["file", fileId],
     queryFn: () => getFile({ fileId }),
   });
 
+  const handleClickDownload = () => {
+    if (file?.presigned_url) {
+      downloadFile(file.presigned_url, file.name);
+    }
+  };
+
   return (
     <div>
-      <button type="button" className="text-[#3B82F6] font-bold">
+      <button
+        type="button"
+        className="text-[#3B82F6] font-bold"
+        onClick={handleClickDownload}
+      >
         <span>{file?.name}</span>
       </button>
     </div>

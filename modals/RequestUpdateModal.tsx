0d015996b36bcbeee0705ff0f3fa2d15c855ea39
GJ <lgj9172@gmail.com>
2025-07-23 20:20:19 +0900

    fix: disable request update button

--- modals/RequestUpdateModal.tsx
+++ modals/RequestUpdateModal.tsx
@@ -61,20 +61,34 @@ export default function RequestUpdateModal({
         <AlertDialogHeader>
           <AlertDialogTitle>번역 수정 요청</AlertDialogTitle>
           <AlertDialogDescription>
-            번역 수정을 요청하시겠어요?
-            <br />
-            번역 수정 요청은 최대 3번 가능해요.
-            <br />
-            {isLoading
-              ? "로딩 중..."
-              : `현재 수정 요청 가능 횟수: ${remainCount}번`}
+            {remainCount === 0 ? (
+              <>
+                번역 수정 요청 가능 횟수를 모두 소진해서 수정 요청을 할 수
+                없어요.
+                <br />
+                번역 수정 요청은 최대 3번 가능해요.
+              </>
+            ) : (
+              <>
+                번역 수정을 요청하시겠어요?
+                <br />
+                번역 수정 요청은 최대 3번 가능해요.
+                <br />
+                {isLoading
+                  ? "로딩 중..."
+                  : `현재 수정 요청 가능 횟수: ${remainCount}번`}
+              </>
+            )}
           </AlertDialogDescription>
         </AlertDialogHeader>
         <AlertDialogFooter>
           <Button variant="secondary" onClick={handleClickCancel}>
             닫기
           </Button>
-          <Button onClick={handleClickConfirm} disabled={isPending}>
+          <Button
+            onClick={handleClickConfirm}
+            disabled={isPending || remainCount === 0}
+          >
             번역 수정 요청
           </Button>
         </AlertDialogFooter>

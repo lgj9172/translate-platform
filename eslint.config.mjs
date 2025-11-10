7eefc77e3308ed6b223ebb6a03aa931d3b68edc0
GJ <lgj9172@gmail.com>
2025-11-10 23:43:32 +0900

    migrate: eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,56 +1,30 @@
-import { defineConfig } from "eslint/config";
-import react from "eslint-plugin-react";
-import typescriptEslint from "@typescript-eslint/eslint-plugin";
-import globals from "globals";
-import tsParser from "@typescript-eslint/parser";
+import nextCoreWebVitals from "eslint-config-next/core-web-vitals";
 import path from "node:path";
 import { fileURLToPath } from "node:url";
-import js from "@eslint/js";
 import { FlatCompat } from "@eslint/eslintrc";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
+  baseDirectory: __dirname,
 });
 
-export default defineConfig([{
-    extends: compat.extends(
-        "eslint:recommended",
-        "plugin:react/recommended",
-        "plugin:@typescript-eslint/recommended",
-        "next/core-web-vitals",
-        "prettier",
-    ),
-
-    plugins: {
-        react,
-        "@typescript-eslint": typescriptEslint,
-    },
-
-    languageOptions: {
-        globals: {
-            ...globals.browser,
-            ...globals.node,
-        },
-
-        parser: tsParser,
-        ecmaVersion: "latest",
-        sourceType: "module",
-
-        parserOptions: {
-            project: ["tsconfig.json"],
-        },
-    },
-
+const config = [
+  ...nextCoreWebVitals,
+  ...compat.extends("prettier"),
+  {
     rules: {
-        "react/react-in-jsx-scope": "off",
-        "react/jsx-props-no-spreading": "off",
-        "react/require-default-props": "off",
-        "import/prefer-default-export": "off",
-        "jsx-a11y/label-has-associated-control": "off",
-        "react/prop-types": "off",
+      "react/react-in-jsx-scope": "off",
+      "react/jsx-props-no-spreading": "off",
+      "react/require-default-props": "off",
+      "import/prefer-default-export": "off",
+      "jsx-a11y/label-has-associated-control": "off",
+      "react/prop-types": "off",
     },
-}]);
+  },
+  {
+    ignores: ["server.js"],
+  },
+];
+
+export default config;
